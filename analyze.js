f440775795557d89e8abca04654f2356570020d8
jared <38332939+jaredaritter@users.noreply.github.com>
2020-12-03 11:28:29 -0500

    Include analyze function and tests

--- analyze.js
+++ analyze.js
@@ -4,7 +4,56 @@ function analyze(array) {
   } else if (array === null) {
     return { msg: 'Argument must be array' };
   }
-  return array;
+  // HANDLE DATA CLARITY HERE OR ELSEWHERE?
+  const avg = average(array);
+  // const min = minimum(array);
+  const min = minimum2(array);
+  // const max = maximum(array);
+  const max = maximum2(array);
+  const len = array.length;
+  return {
+    average: avg,
+    min: min,
+    max: max,
+    length: len,
+  };
+}
+
+function average(array) {
+  const sum = array.reduce((a, c) => a + c);
+  return sum / array.length;
+}
+
+// LOOP BASED VERSIONS
+function minimum(array) {
+  let min = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
+}
+
+function maximum(array) {
+  let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+}
+
+// ARRAY.SORT BASED  VERSIONS
+function minimum2(array) {
+  const ascending = array.sort((a, b) => (a < b ? -1 : 1));
+  return ascending[0];
+}
+
+function maximum2(array) {
+  const descending = array.sort((a, b) => (a > b ? -1 : 1));
+  return descending[0];
 }
 
 module.exports = analyze;
--- analyze.test.js
+++ analyze.test.js
@@ -13,3 +13,45 @@ test('Should handle non-string arguments by returning object with error message'
   expect(analyze(undefined)).toStrictEqual({ msg: 'Argument must be array' });
   expect(analyze(null)).toStrictEqual({ msg: 'Argument must be array' });
 });
+
+test('Should return object with correct values', () => {
+  expect(analyze([1, 2, 3])).toStrictEqual({
+    average: 2,
+    min: 1,
+    max: 3,
+    length: 3,
+  });
+  expect(analyze([1, 2, 3, 4, 5])).toStrictEqual({
+    average: 3,
+    min: 1,
+    max: 5,
+    length: 5,
+  });
+  expect(analyze([1, 3, 3, 18, 46])).toStrictEqual({
+    average: 14.2,
+    min: 1,
+    max: 46,
+    length: 5,
+  });
+});
+
+test('Should work with negative numbers and zero', () => {
+  expect(analyze([1, 2, 3, 4, -5])).toStrictEqual({
+    average: 1,
+    min: -5,
+    max: 4,
+    length: 5,
+  });
+  expect(analyze([0, 2, 3, 4, -4])).toStrictEqual({
+    average: 1,
+    min: -4,
+    max: 4,
+    length: 5,
+  });
+  expect(analyze([-4, 4, 3, 2, 0])).toStrictEqual({
+    average: 1,
+    min: -4,
+    max: 4,
+    length: 5,
+  });
+});
